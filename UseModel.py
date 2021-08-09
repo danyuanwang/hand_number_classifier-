225ce47d199d7e3cac30900111d5a31804624d20
danyuanwang <danyuanwang@hotmail.com>
2021-08-08 22:26:25 -0700

    changes to usemodel debugging pending

--- UseModel.py
+++ UseModel.py
@@ -8,7 +8,7 @@ from tensorflow.keras.layers import Flatten, Dense, Conv2D, MaxPooling2D
 
 CURRENT_FOLDER = os.getcwd()
 #tf.saved_model.LoadOptions(experimental_io_device = "/job:localhost")
-model = tf.keras.models.load_model("mlmodel")
+
 print(CURRENT_FOLDER + "image.PNG")
 
 def create_model():
@@ -21,6 +21,7 @@ def create_model():
 
 def loadModel(file):
     model = create_model()
+    model = tf.keras.models.load_model("mlmodel")
     ModuleNotFoundError()
 
 
@@ -28,3 +29,7 @@ def compressFile(file):
     img = Image.open(CURRENT_FOLDER + file)
     img = img.resize((256, 256))
     return img
+model = loadModel("mlmodel")
+img = compressFile()
+prediction = model.predict(img)
+print(prediction)
