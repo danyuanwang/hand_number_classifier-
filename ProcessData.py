5059c31ffe9487cc5dc018ace4c000e3dde4a70b
danyuanwang <danyuanwang@hotmail.com>
2021-08-10 11:01:01 -0700

    nothng

--- ProcessData.py
+++ ProcessData.py
@@ -15,15 +15,15 @@ for image in os.listdir(os.path.join(os.getcwd(), "archive/test")):
     img = io.imread(os.path.join(os.getcwd(), directory))
     randomAngle = (random.random() *360) + 1
     RotatedImg = rotate(img, angle = randomAngle, mode = 'wrap')
-    randomXShift = (random.random() * 100) - 50
-    randomYShift = (random.random() * 100) - 50
+    randomXShift = (random.random() * 60) - 30
+    randomYShift = (random.random() * 60) - 30
     #print(randomXShift, randomYShift)
     transform = AffineTransform(translation = (randomXShift, randomYShift))
     transformedImg = warp(RotatedImg,transform,mode='wrap')
     sigma = 0.2
     noisyImg = random_noise(transformedImg, var = sigma ** 2)
     #blurredImg  = filters.gaussian(image,sigma=1,multichannel=True)
-    io.imsave("archive/AugmentedTest/augmentedTest" + str(number) + ".png", RotatedImg)
+    io.imsave("archive/AugmentedTest/augmentedTest" + str(number) + ".png", noisyImg)
     number += 1
     print(image + "  " +  str(number))
     break
