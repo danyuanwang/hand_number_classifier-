079bd9effef330bf93e8b57cbbe91ebd14c663e7
danyuanwang <danyuanwang@hotmail.com>
2021-08-14 13:14:27 -0700

    all

--- ProcessData.py
+++ ProcessData.py
@@ -8,15 +8,14 @@ import cv2
 import random
 print(os.getcwd())
 number = 0
-for image in os.listdir(os.path.join(os.getcwd(), "archive/train")):
+for image in os.listdir(os.path.join(os.getcwd(), "archive/test")):
     
     
-    directory = os.path.join("archive/train", image)
-    img = io.imread(os.path.join(os.getcwd(), directory))
-    #print(img.shape)
-    img = resize(img, (224, 224))
-    #print(img.shape)
-    
+    directory = os.path.join("archive/test", image)
+    img = cv2.imread(os.path.join(os.getcwd(), directory))
+    #grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #(thresh, BWImg) = cv2.threshold(grayImg, 127, 255, cv2.THRESH_BINARY)
+
     randomAngle = (random.random() *360) + 1
     RotatedImg = rotate(img, angle = randomAngle, mode = 'wrap')
     randomXShift = (random.random() * 60) - 30
@@ -24,13 +23,16 @@ for image in os.listdir(os.path.join(os.getcwd(), "archive/train")):
     #print(randomXShift, randomYShift)
     transform = AffineTransform(translation = (randomXShift, randomYShift))
     transformedImg = warp(RotatedImg,transform,mode='wrap')
-    sigma = 0.1
+    
+
+    #sigma = 0.1
     #noisyImg = random_noise(transformedImg, var = sigma ** 2)
     #blurredImg  = filters.gaussian(image,sigma=1,multichannel=True)
     FinalImg = cv2.normalize(transformedImg, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-    io.imsave("archive/AugmentedTrain/a" + image, FinalImg)
+    #print(FinalImg.shape)
+    cv2.imwrite("archive/AugmentedTest/a" + image, FinalImg)
     number += 1
     print(image + "  " +  str(number))
-    #break
+
     
     
--- h2vgg19.py
+++ h2vgg19.py
@@ -8,11 +8,19 @@ import cv2
 from tensorflow.keras.applications.vgg19 import VGG19
 
 CURRENT_FOLDER = os.getcwd()
+def processImage(img):
+    grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    (thresh, BWImg) = cv2.threshold(grayImg, 127, 255, cv2.THRESH_BINARY)
+    FinalImg = cv2.normalize(BWImg, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+    #print(BWImg.shape)
+    return FinalImg
+
 def createData(directory):
     dataX = []
     dataY = []
     for img in os.listdir(directory):
         imgArr = cv2.imread(os.path.join(directory, img))
+        imgArr = processImage(imgArr)
         imgArr = cv2.resize(imgArr, (224, 224))
         numFing = int(img.split('_')[1][0])
         dataX.append(imgArr)
@@ -23,7 +31,7 @@ def createData(directory):
 
 
 
-trainDir = CURRENT_FOLDER + "/archive/train"
+trainDir = CURRENT_FOLDER + "/archive/AugmentedTrain"
 trX, trY = createData(trainDir)
 '''
 plt.imshow(trX[3534], cmap = 'gray')
@@ -38,7 +46,7 @@ print(trY.shape)
 
 #------------------------------------------------
 
-testDir =CURRENT_FOLDER + "/archive/test"
+testDir =CURRENT_FOLDER + "/archive/AugmentedTest"
 teX, teY = createData(testDir)
 
 print(teX.shape)
@@ -48,9 +56,9 @@ print(teY.shape)
 
 #-----------------------------------------------
 feature_extractor = VGG19(
-    input_shape=(224, 224, 3),
+    input_shape=(224, 224, 1),
     include_top=False,
-    weights='imagenet',
+    weights=  None,#'imagenet',
     pooling='avg'
 )
 
@@ -72,4 +80,4 @@ model.fit(trX, trY, epochs=1, batch_size = 32)
 loss, acc = model.evaluate(teX, teY)
 print('\ntest_accuracy: ' + str(acc))
 
-model.save("regular.model")
+model.save("BW.model")
